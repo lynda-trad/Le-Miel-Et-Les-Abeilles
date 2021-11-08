5322ce8f6821f04a164d4878213bc9a5deb4d7f5
lynda-trad <lynda.trad@laplateforme.io>
2021-11-08 12:00:12 +0100

    crossing children = half of both parents side by side + remove duplicates + add missing flowers to the end -> needs testing

--- main.py
+++ main.py
@@ -64,16 +64,37 @@ def sortPopulation(fitnesses, population):
 
 
 # Crosses first half and last half of two genomes ; returns new genome
-def crossing(first_half, last_half, half):
+def crossing(first_half, last_half, half, flowList):
     cross = Path.Path()
     for i in range(half):
         cross.addFlower(first_half[i])
-    for i in range(half, FLOWERS_NUMBER):
+    for i in range(half, FLOWERS_NUMBER - 1):
         cross.addFlower(last_half[i])
+    # Removes duplicates
+    cross.setOrder(list(dict.fromkeys(cross.getOrder())))
+    # Adds missing flowers
+    indexList = []
+    for flower in cross.getOrder():
+        indexList.append(flower.getIndex())
+    for i in range(49):
+        if i not in indexList:
+            missingFlower = getFlowerById(flowList, i)
+            cross.addFlower(missingFlower)
     cross.calculateLength()
     return cross
 
 
+def crossover(population, bestL, flowList):
+    half = int(FLOWERS_NUMBER / 2) - 1
+    orders = []
+    for best in bestL:
+        orders.append(best.getOrder())
+    for i in range(len(bestL) - 3):
+        population.append(crossing(orders[i], orders[i + 2], half, flowList))
+    return population
+
+
+"""
 def crossover(population, bestL):
     half = int(FLOWERS_NUMBER / 2) - 1
     orders = []
@@ -88,6 +109,7 @@ def crossover(population, bestL):
                 population.append(cross)
                 # population.append(crossing(orders[i], orders[j], half))
     return population
+"""
 
 
 # 2 flowers switch places in every path of the population
@@ -99,24 +121,25 @@ def mutation(population):
         while pos2 == pos1:
             pos2 = random.randint(0, FLOWERS_NUMBER - 2)
         path = individual.getOrder()
-        first = path[pos1]
-        second = path[pos2]
+        first_flower = path[pos1]
+        second_flower = path[pos2]
         path.pop(pos1)
         path.pop(pos2)
-        path.insert(pos1, second)
-        path.insert(pos2, first)
+        path.insert(pos1, second_flower)
+        path.insert(pos2, first_flower)
     return population
 
 
+# Removes worst individuals to reach max number of individuals
 def removingWorst(new):
     i = len(new) - 1
-    while i != 99:
+    while i != POPULATION_COUNT - 1:
         new.pop(i)
         i -= 1
     return new
 
 
-# Generates individuals in a population
+# Generates random individuals for the very first generation
 def generatePopulation(population, flowList, count):
     for pop in range(count):
         individual = Path.Path()
@@ -131,23 +154,22 @@ def generatePopulation(population, flowList, count):
 # Generates first generation
 def generateFirstGeneration(fList):
     first = []
-    first = generatePopulation(first, fList, POPULATION_COUNT - len(first))  # 1 )
-    fitnessDic = fitness(first)  # 2 )
+    first = generatePopulation(first, fList, POPULATION_COUNT - len(first))
+    fitnessDic = fitness(first)
     fitnessDic = sortFitnesses(fitnessDic)
     first = sortPopulation(fitnessDic, first)
     return first
 
 
 # Generates a new generation with previous one
-def generateNewGeneration(previous):
+def generateNewGeneration(previous, flowList):
     new = previous
     bestList = []
     for i in range(20):
         bestList.append(new[i])
 
-    new = mutation(new)  # 6 ) 80 other members
-
-    new = crossover(new, bestList)  # 4 ) added 2 children per couple so 20
+    new = mutation(new)  # 6 ) last 80 members will be mutated
+    new = crossover(new, bestList, flowList)  # 4 ) 400 children are generated
 
     # Sorting population
     new_fitnessDic = fitness(new)  # 2 )
@@ -160,7 +182,7 @@ def generateNewGeneration(previous):
 
 
 # Generates GENERATION_COUNT_MAX generations and returns the last one
-def cycle(fList, graph, nodePos):
+def cycle(fList, graph, nodePos, flowList):
     averageList = []
     # First generation
     firstPopulation = generateFirstGeneration(fList)
@@ -171,7 +193,7 @@ def cycle(fList, graph, nodePos):
 
     # New generation
     for i in range(2, GENERATION_COUNT_MAX + 1):
-        newGeneration = generateNewGeneration(previousGen)
+        newGeneration = generateNewGeneration(previousGen, flowList)
         averageList.append(calculateAverage(newGeneration))
         """
         # Printing best bee of this generation
@@ -211,7 +233,7 @@ flowersList = initFlowersList(data)
 G, pos = graphPrinting.initPrintingGraph(flowersList)
 
 print("-- GENERATIONS --\nBe patient !")
-last_generation, averageL = cycle(flowersList, G, pos)
+last_generation, averageL = cycle(flowersList, G, pos, flowersList)
 best_path = last_generation[0]
 printPopulation(last_generation, GENERATION_COUNT_MAX)
 print("\nBEST PATH OF LAST GENERATION : ")
@@ -220,8 +242,3 @@ print("LENGTH", len(last_generation))
 
 # Generates graph showing averageL of each generation compared to the others
 graphPrinting.printAverageGraph(averageL)
-
-"""
-20 mere 10 mere 20 pere
-20 pere 10 pere 20 mere
-"""
